d1fb81a3fcf99115d5390923676ca4c5909b8821
Maycow Moura <maycowmoura@gmail.com>
2022-01-28 20:57:04 -0300

    feat: added api routes

---
+++ backend/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+$router = new \Bramus\Router\Router();
+
+$router->get('/plans', function () {
+  require_once __DIR__ . '/controllers/get-plans.php';
+});
+
+$router->post('/budgets', function () {
+  require_once __DIR__ . '/controllers/get-budget.php';
+});
+
+$router->set404(function () {
+  echo "<h1>404</h1>";
+});
+
+$router->run();
